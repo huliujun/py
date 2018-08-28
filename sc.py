d589486e67878a999ca5fb7c270a6e0aff368ecc
unknown <hulj@kingnet.sh>
2018-08-28 10:21:21 +0800

    any

--- sc.py
+++ sc.py
@@ -4,6 +4,9 @@ import os
 import sys
 import requests
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 # 深度
 skip = 1
 # 第几个开始
@@ -16,6 +19,8 @@ if len(sys.argv) == 3:
     skip = int(sys.argv[1])
     start = int(sys.argv[2])
 
+
+
 r = requests.get(
     'https://service.videowp.adesk.com/v1/aibizhi/category?appid=com.lovebizhi.ipad&appver=5.1&appvercode=64&channel=ipicture&lan=zh-Hans-CN&sys_model=iPhone&sys_name=iOS&sys_ver=11.4.1')
 # print type(r.json())
@@ -23,28 +28,50 @@ r = requests.get(
 imgListURL = 'https://service.videowp.adesk.com/v1/aibizhi/videowp?appid=com.lovebizhi.ipad&appver=5.1&appvercode=64&channel=ipicture&lan=zh-Hans-CN&limit=30&sys_model=iPhone&sys_name=iOS&sys_ver=11.4.1'
 
 # 字符串的字典 转 字典类型 1、json(), 不支持单引号，2、veal()说可能存在安全问题 3、literal_eval不存在上述问题
-for re in r.json()['res']['category']:
 
-    # print re['name']
-    # print re['id'] 
+def mkdir(path):
+    # 去除首位空格
+    path = path.strip()
+    # 去除尾部 \ 符号
+    path = path.rstrip("\\")
 
-    if re['name'] != u'1':
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists = os.path.exists(path)
 
-        count = 1
-        for i in range(skip):
+    # 判断结果
+    if not isExists:
+        # 如果不存在则创建目录
+        # 创建目录操作函数
+        os.makedirs(path)
 
-            parm = {'category': re['id'], 'skip': str(i * 30)}
-            listR = requests.get(imgListURL, params=parm)
-            # print type(listR.json())
-            for listRe in listR.json()['res']['videowp']:
-                if count >= 416:
-                    print '正在偷偷下载第 ' + str(count) + '个mm视频'
-                    with open(re['name'] + '_' + listRe['name'] + '.mp4', 'wb') as f:
-                        f.write(requests.get(listRe['video']).content)
-                else:
-                    print '第' + str(count) + '个mm视频跳过'
-                count += 1
+        print path + u' 创建成功'
+        return True
     else:
-        print re['name'] + u'分类跳过'
+        # 如果目录存在则不创建，并提示目录已存在
+        print path + u' 目录已存在'
+        return False
 
-print '下载完成'
+count = 1
+
+for i in range(skip):
+
+    for re in r.json()['res']['category']:
+        if i == 0:
+            # mkdir(u'../mp4/video/' + re['name'])
+            mkdir(u'../mp4/view_video/' + re['name'])
+        parm = {'category': re['id'], 'skip': str(i * 30)}
+        listR = requests.get(imgListURL, params=parm)
+        for listRe in listR.json()['res']['videowp']:
+            if count >= start:
+                print u'正在偷偷下载第 ' + str(count) + u' 个视频， 分类为：' + re['name'] + u'， 深度为：' + str(i)
+                # with open(u'../mp4/video/' + re['name'] + '/' + re['name'] + '_' + listRe['id'] + '.mp4', 'wb') as f:
+                #     f.write(requests.get(listRe['video']).content)
+                with open(u'../mp4/view_video/' + re['name'] + '/' + re['name'] + '_' + listRe['id'] + '.mp4', 'wb') as f:
+                    f.write(requests.get(listRe['view_video']).content)
+            else:
+                print '第' + str(count) + u' 个视频跳过， 分类为：' + re['name'] + u'， 深度为：' + str(i)
+            count += 1
+
+print u'下载完成'
--- scimg.py
+++ scimg.py
@@ -20,6 +20,13 @@ if len(sys.argv) == 3:
     start = int(sys.argv[2])
 
 
+r = requests.get(
+    'http://service.aibizhi.adesk.com/v1/lightwp/category?appid=com.lovebizhi.ipad&appver=5.1&appvercode=64&channel=ipicture&lan=zh-Hans-CN&sys_model=iPhone&sys_name=iOS&sys_ver=11.4.1')
+
+imgListURL = 'http://service.aibizhi.adesk.com/v1/vertical/category/4e4d610cdf714d2966000000/vertical?adult=0&appid=com.lovebizhi.ipad&appver=5.1&appvercode=64&channel=ipicture&first=1&lan=zh-Hans-CN&limit=30&skip=0&sys_model=iPhone&sys_name=iOS&sys_ver=11.4.1'
+
+# 字符串的字典 转 字典类型 1、json(), 不支持单引号，2、veal()说可能存在安全问题 3、literal_eval不存在上述问题
+
 def mkdir(path):
     # 去除首位空格
     path = path.strip()
@@ -45,35 +52,30 @@ def mkdir(path):
         return False
 
 
-r = requests.get(
-    'http://service.aibizhi.adesk.com/v1/lightwp/category?appid=com.lovebizhi.ipad&appver=5.1&appvercode=64&channel=ipicture&lan=zh-Hans-CN&sys_model=iPhone&sys_name=iOS&sys_ver=11.4.1')
+count = 1
+# 循环深度
+for i in range(skip):
+    # 得到分类id
+    for re in r.json()['res']['category']:
+        # 循环深度首次创建文件夹
+        if i==0:
+            mkdir(u'../resource/' + re['name'])
+        # 组合链接
+        listR = requests.get('http://service.aibizhi.adesk.com/v1/vertical/category/' + re[
+            'id'] + '/vertical?adult=0&appid=com.lovebizhi.ipad&appver=5.1&appvercode=64&channel=ipicture&first=1&lan=zh-Hans-CN&limit=30&skip=' + str(
+            i * 30) + '&sys_model=iPhone&sys_name=iOS&sys_ver=11.4.1')
+        # print type(listR.json())
+        for listRe in listR.json()['res']['vertical']:
+            if count >= start:
+                print u'正在偷偷下载第 ' + str(count) + u' 张图片 分类为：' + re['name'] + u' 深度为：' + str(i)
+                with open(u'../resource/' + re['name'] + '/' + re['name'] + '_' + listRe['id'] + '.jpg', 'wb') as f:
+                    f.write(requests.get(listRe['img']).content)
+            else:
+                print '第' + str(count) + u' 张图片跳过 分类为：' + re['name'] + u' 深度为：' + str(i)
+            count += 1
 
-imgListURL = 'http://service.aibizhi.adesk.com/v1/vertical/category/4e4d610cdf714d2966000000/vertical?adult=0&appid=com.lovebizhi.ipad&appver=5.1&appvercode=64&channel=ipicture&first=1&lan=zh-Hans-CN&limit=30&skip=0&sys_model=iPhone&sys_name=iOS&sys_ver=11.4.1'
+print u'下载完成'
 
-# 字符串的字典 转 字典类型 1、json(), 不支持单引号，2、veal()说可能存在安全问题 3、literal_eval不存在上述问题
-for re in r.json()['res']['category']:
 
-    # print re['name']
-    # print re['id'] 
-    mkdir(u'../resource/' + re['name'])
-    if re['name'] != u'1':
 
-        # parm = {'category':re['id']}
-        count = 1
-        for i in range(skip):
-            listR = requests.get('http://service.aibizhi.adesk.com/v1/vertical/category/' + re[
-                'id'] + '/vertical?adult=0&appid=com.lovebizhi.ipad&appver=5.1&appvercode=64&channel=ipicture&first=1&lan=zh-Hans-CN&limit=30&skip=' + str(
-                i * 30) + '&sys_model=iPhone&sys_name=iOS&sys_ver=11.4.1')
-            # print type(listR.json()) 
-            for listRe in listR.json()['res']['vertical']:
-                if count >= start:
-                    print u'正在偷偷下载第 ' + str(count) + u'张' + re['name'] + u'图片'
-                    with open(u'../resource/' + re['name'] + '/' + re['name'] + '_' + listRe['id'] + '.jpg', 'wb') as f:
-                        f.write(requests.get(listRe['img']).content)
-                else:
-                    print '第' + str(count) + '个mm图片跳过'
-                count += 1
-    else:
-        print re['name'] + u'分类跳过'
 
-print '下载完成'
