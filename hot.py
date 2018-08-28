3dd67f40e34e3b8c55b3d89e1554b0607259cb35
huliujun <1059173751@qq.com>
2018-08-28 23:40:28 +0800

    调整hot

--- hot.py
+++ hot.py
@@ -1,36 +1,22 @@
 # -*- coding: utf8 -*-
 
-import os
 import sys
 import MySQLdb
 import urllib2
 from bs4 import BeautifulSoup
 import datetime
-import lxml
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-
-def get_soup(url):
-    a_all=[]
-    res = urllib2.urlopen(url)
-    html = res.read()
-    soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
-    p = soup.find_all('p', attrs={'class': 'developer'})
-    for a in p:
-        a_all.append(a.find('a'))
-    return a_all
-
-
+# 获取参数，默认今天
 if len(sys.argv) == 1:
     now = datetime.datetime.now()
     date = now.strftime('%Y%m%d')
 else:
     date = sys.argv[1]
 
-print date
-
+# 连接数据库
 conn = MySQLdb.connect(
     host='localhost',
     port=3306,
@@ -41,8 +27,19 @@ conn = MySQLdb.connect(
 )
 cur = conn.cursor()
 conn.autocommit(1)  # 数据库自动提交
-j = 1
 
+# 爬虫获取块
+def get_soup(url):
+    a_all=[]
+    res = urllib2.urlopen(url)
+    html = res.read()
+    soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
+    p = soup.find_all('p', attrs={'class': 'developer'})
+    for a in p:
+        a_all.append(a.find('a'))
+    return a_all
+
+# 爬取信息配置
 json_config = [
     {'free':1,'type':0,'name':u'免费总榜','url':'https://www.chandashi.com/home/ranking/index/type/free/genre/0/country/cn.html?date=' + date},
     {'free':1,'type':1,'name':u'免费游戏榜','url':'https://www.chandashi.com/home/ranking/index/type/free/genre/6014/country/cn.html?date=' + date},
@@ -51,6 +48,7 @@ json_config = [
     {'free':0,'type':0,'name':u'畅销游戏榜','url':'https://www.chandashi.com/home/ranking/index/type/grossing/genre/6014/country/cn.html?date=' + date}
 ]
 
+# 循环爬取
 for value in json_config:
     tmp = []
     i = 1
@@ -68,3 +66,4 @@ for value in json_config:
     tup = tuple(tmp)
     cur.executemany("insert into my_db.appstore_hot_bak (name,corp,rank,free,type,date) values(%s,%s,%s,%s,%s,%s)", tup)
     print value['name'] + u'导入成功'
+print u'脚本运行完毕'
